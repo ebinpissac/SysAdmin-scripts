c9e1fe06704f26a6056d288cddd325540419324f
ebinpissac <ebinpissac@gmail.com>
2018-03-11 14:11:59 +0800

    export ad users and group memberships to csv

---
+++ ExportGroupMembershipstoCSV.ps1
@@ -0,0 +1,6 @@
+﻿$users = Get-ADUser -Filter * 
+foreach ($user in $users) {
+
+$Groups = (Get-ADPrincipalGroupMembership -Identity $user.SamAccountName | Select-Object -ExpandProperty name) -join ','
+get-aduser $user.SamAccountName -properties memberof,samaccountname,givenname,surname | select samaccountname, @{name="Groups";expression={$Groups}} | export-csv -append "ADUsers.csv" -Delimiter "," -NoTypeInformation -Encoding UTF8
+}
